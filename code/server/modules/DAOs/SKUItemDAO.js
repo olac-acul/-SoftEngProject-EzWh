3ecc27df4d1a4f105b3d39904f9c6b480f57c13f
SeyedOmid Mahdavi s299837 <s299837@studenti.polito.it>
2022-05-11 10:19:00 +0200

    Implemented all SKUItem APIs

--- code/server/modules/DAOs/SKUItemDAO.js
+++ code/server/modules/DAOs/SKUItemDAO.js
@@ -34,6 +34,68 @@ class SKUItemDAO {
         });
     }
 
+    getAllSKUItems() {
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT * FROM SKU_ITEMS';
+            this.db.all(sql, [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                const SKUItems = rows.map((r) => (
+                    {
+                        RFID: r.RFID,
+                        SKUId: r.SKU_ID,
+                        available: r.AVAILABLE,
+                        dateOfStock: r.DATE_OF_STOCK
+                    }
+                ));
+                resolve(SKUItems);
+            });
+        });
+    }
+
+    getAllAvailableSKUItems(id) {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT RFID, SKU_ID, DATE_OF_STOCK
+                         FROM SKU_ITEMS
+                         WHERE SKU_ID = ? AND AVAILABLE = TRUE`;
+            this.db.all(sql, [id], (err, rows) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                const SKUItems = rows.map((r) => (
+                    {
+                        RFID: r.RFID,
+                        SKUId: r.SKU_ID,
+                        dateOfStock: r.DATE_OF_STOCK
+                    }
+                ));
+                resolve(SKUItems);
+            });
+        });
+    }
+
+    getSKUItem(RFID) {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT * FROM SKU_ITEMS WHERE RFID = ?`;
+            this.db.get(sql, [RFID], (err, row) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                const SKUItem = {
+                    RFID: row.RFID,
+                    SKUId: row.SKU_ID,
+                    available: row.AVAILABLE,
+                    dateOfStock: row.DATE_OF_STOCK
+                };
+                resolve(SKUItem);
+            });
+        });
+    }
+
     addSKUItem(SKUItem) {
         return new Promise((resolve, reject) => {
             const sql = 'INSERT INTO SKU_ITEMS(RFID, SKU_ID, DATE_OF_STOCK, AVAILABLE) VALUES(?, ?, ?, 0)';
@@ -43,6 +105,34 @@ class SKUItemDAO {
                     return;
                 }
                 resolve(this.lastID);
+            });
+        });
+    }
+
+    modifySKUItem(oldRFID, newRFID, newAvailable, newDateOfStock) {
+        return new Promise((resolve, reject) => {
+            const sql = `UPDATE SKU_ITEMS
+                         SET RFID = ?, AVAILABLE = ?, DATE_OF_STOCK = ?
+                         WHERE RFID = ?`;
+            this.db.run(sql, [newRFID, newAvailable, newDateOfStock, oldRFID], function (err) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(this.changes);
+            });
+        });
+    }
+
+    deleteSKUItem(RFID) {
+        return new Promise((resolve, reject) => {
+            const sql = 'DELETE FROM SKU_ITEMS WHERE RFID = ?';
+            this.db.run(sql, [RFID], function (err) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(this.changes);
             });
         });
     }
--- code/server/modules/SKUItem.js
+++ code/server/modules/SKUItem.js
@@ -3,6 +3,38 @@ function SKUItemAPIs(app) {
 
     const skuItemDAO = new SKUItemDAO();
 
+    // GET
+    app.get('/api/skuitems', async (req, res) => {
+        try {
+            // 401 Unauthorized (not logged in or wrong permissions)
+            const SKUItems = await skuItemDAO.getAllSKUItems();
+            res.status(200).json(SKUItems);
+        } catch (err) {
+            res.status(500).json({ error: `Generic error` }).end();
+        }
+    });
+
+    app.get('/api/skuitems/sku/:id', async (req, res) => {
+        try {
+            const id = req.params.id;
+            // 401 Unauthorized (not logged in or wrong permissions)
+            const SKUItems = await skuItemDAO.getAllAvailableSKUItems(id);
+            res.status(200).json(SKUItems);
+        } catch (err) {
+            res.status(500).json({ error: `Generic error` }).end();
+        }
+    });
+
+    app.get('/api/skuitems/:rfid', async (req, res) => {
+        try {
+            const RFID = req.params.rfid;
+            // 401 Unauthorized (not logged in or wrong permissions)
+            const SKUItem = await skuItemDAO.getSKUItem(RFID);
+            res.status(200).json(SKUItem);
+        } catch (err) {
+            res.status(500).json({ error: `Generic error` }).end();
+        }
+    });
 
 
     // POST
@@ -22,6 +54,38 @@ function SKUItemAPIs(app) {
             await skuItemDAO.addSKUItem(SKUItem);
             return res.status(201).end();
 
+        } catch (err) {
+            res.status(503).json({ error: `Generic error` }).end();
+        }
+    });
+
+
+    // PUT
+    app.put('/api/skuitems/:rfid', async (req, res) => {
+        try {
+            const oldRFID = req.params.rfid;
+            const newRFID = req.body.newRFID;
+            const newAvailable = req.body.newAvailable;
+            const newDateOfStock = req.body.newDateOfStock;
+            updatedElements = await skuItemDAO.modifySKUItem(oldRFID, newRFID, newAvailable, newDateOfStock);
+            if (updatedElements === 0) return res.status(404).json({ error: `No SKU Item associated to RFID` }).end();
+            return res.status(200).end();
+        } catch (err) {
+            res.status(503).json({ error: `Generic error` }).end();
+        }
+    });
+
+
+    //DELETE
+    app.delete('/api/skuitems/:rfid', async (req, res) => {
+        try {
+            const RFID = req.params.rfid;
+            // 401 Unauthorized (not logged in or wrong permissions)
+            const deletedSKUItems = await skuItemDAO.deleteSKUItem(RFID);
+            // Check id validation
+            if (deletedSKUItems === 0) res.status(422).json({ error: `Validation of RFID failed` }).end();
+            // END OF VALIDATION
+            res.status(204).end();
         } catch (err) {
             res.status(503).json({ error: `Generic error` }).end();
         }
