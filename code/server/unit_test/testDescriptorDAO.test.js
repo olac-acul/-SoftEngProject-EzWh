e2db561f9cf7964f50ad845f62a96a7bee6dd7a0
Arefeh Mohammad Nejad s301416 <s301416@studenti.polito.it>
2022-05-18 14:41:48 +0430

    tests modified

--- code/server/unit_test/testDescriptorDAO.test.js
+++ code/server/unit_test/testDescriptorDAO.test.js
@@ -11,6 +11,7 @@ describe('testTestDescriotorDao', () => {
     });
 
     testCreateTestDescriptor_And_GetTestDescriptors_And_GetTestDescriptorById(1, "test descriptor 1", "PD1", 8)
+    testModifyTestDescriptor("test descriptor 1", "PD1", 8, 1, "test descriptor 1", "PD2", 26);
     testDeleteTestDescriptor(1, "test descriptor 1", "PD1", 8);
 });
 
@@ -24,6 +25,19 @@ function testCreateTestDescriptor_And_GetTestDescriptors_And_GetTestDescriptorBy
         expect(res.name).toStrictEqual(name);
         expect(res.procedureDescription).toStrictEqual(procedureDescription);
         expect(res.idSKU).toStrictEqual(idSKU);
+    });
+}
+
+function testModifyTestDescriptor(name, procedureDescription, idSKU, id, newStatus){
+    test("search SKU", async () => {
+        await testDescriptorDAO.createTestDescriptor(name, procedureDescription, idSKU);
+        await testDescriptorDAO.modifyTestDescriptor(id, newStatus);   
+        var res = await testDescriptorDAO.getTestDescriptorById();
+        expect(res.length).toStrictEqual(1);
+        expect(res.id).toStrictEqual(id);
+        expect(res.name).toStrictEqual(newStatus.name);
+        expect(res.procedureDescription).toStrictEqual(newStatus.procedureDescription);
+        expect(res.idSKU).toStrictEqual(newStatus.idSKU);
     });
 }
 
