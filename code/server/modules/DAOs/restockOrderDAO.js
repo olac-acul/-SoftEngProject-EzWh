a68f75d981a3278bede4ad06fa07966676601a81
SeyedOmid Mahdavi s299837 <s299837@studenti.polito.it>
2022-05-21 16:46:00 +0200

    Validated restockOrder API requests

--- code/server/modules/DAOs/restockOrderDAO.js
+++ code/server/modules/DAOs/restockOrderDAO.js
@@ -21,7 +21,20 @@ exports.dropTable = () => {
 exports.newRestockOrderTable = () => {
     return new Promise((resolve, reject) => {
         const sql = `CREATE TABLE IF NOT EXISTS RESTOCK_ORDERS(ID INTEGER PRIMARY KEY AUTOINCREMENT, 
-                ISSUE_DATE DATETIME, STATE TEXT, SUPPLIER_ID INTEGER, TRANSPORT_NOTE INTEGER)`;
+                ISSUE_DATE DATETIME, STATE TEXT, SUPPLIER_ID INTEGER, TRANSPORT_NOTE DATE)`;
+        db.run(sql, function (err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(this.lastID);
+        });
+    });
+}
+
+exports.newRestockOrder_join_ProductTable = () => {
+    return new Promise((resolve, reject) => {
+        const sql = `CREATE TABLE IF NOT EXISTS RESTOCK_ORDER_ITEM(RESTOCK_ORDER_ID INTEGER PRIMARY KEY, ITEM_ID INTEGER PRIMARY KEY, QUANTITY INTEGER)`;
         db.run(sql, function (err) {
             if (err) {
                 reject(err);
@@ -34,89 +47,21 @@ exports.newRestockOrderTable = () => {
 
 exports.getRestockOrders = () => {
     return new Promise((resolve, reject) => {
-        // duoble join, 3 tables to get the list of SKU
-        const sql = `SELECT RO.ISSUE_DATE ISSUE_DATE, RO.ID ID, RO.STATE STATE, RO.SUPPLIER_ID SUPPILER_ID, RO.TRANSPORT_NOTE TRANSPORT_NOTE, P.DESCRIPTION DESCRIPTION, P.PRICE PRICE, P.QUANTITY QUANTITY, P.SKU_ID SKU_ID, SI.RFID RFID
-            FROM RESTOCK_ORDERS AS RO, SKU_ITEMS AS SI, (
-                SELECT I.DESCRIPTION DESCRIPTION, I.PRICE PRICE, ROI.QUANTITY QUANTITY, I.SKU_ID SKU_ID, ROI.RESTOCK_ORDER_ID RESTOCK_ORDER_ID
-                FROM ITEMS AS I, RESTOCK_ORDER_ITEM AS ROI
-                WHERE I.ID = ROI.ITEM_ID) AS P
-            WHERE P.SKU_ID = SI.SKU_ID AND RO.ID = P.RESTOCK_ORDER_ID`;
+        const sql = `SELECT * FROM RESTOCK_ORDERS`;
         db.all(sql, [], (err, rows) => {
             if (err) {
                 reject(err);
                 return;
             }
-            const restockOrders = [];
-            let products = [];
-            let skuItems = [];
-            let id = 0;
-            let issueDate;
-            let state;
-            let supplierId;
-            let transportNote = {
-                deliveryDate: null
-            };
-            for (let r in rows) {
-                if (id === 0) {
-                    id = rows[r].ID;
-                    issueDate = rows[r].ISSUE_DATE;
-                    supplierId = rows[r].SUPPLIER_ID;
-                    state = rows[r].STATE;
-                    transportNote.deliveryDate = rows[r].TRANSPORT_NOTE;
-                    products.push({
-                        SKUId: rows[r].SKU_ID,
-                        description: rows[r].DESCRIPTION,
-                        price: rows[r].PRICE,
-                        quantity: rows[r].QUANTITY
-                    });
-                    skuItems.push({
-                        skuId: rows[r].skuId,
-                        rfid: rows[r].rfid
-                    })
+            const restockOrders = rows.map((r) => (
+                {
+                    id: r.ID,
+                    issueDate: r.ISSUE_DATE,
+                    state: r.STATE,
+                    transportNote: r.TRANSPORT_NOTE,
+                    supplierId: r.SUPPILER_ID
                 }
-                else if (id === rows[r].ID) {
-                    products.push({
-                        SKUId: rows[r].SKU_ID,
-                        description: rows[r].DESCRIPTION,
-                        price: rows[r].PRICE,
-                        quantity: rows[r].QUANTITY
-                    });
-                }
-                else if (id !== rows[r].ID) {
-                    restockOrders.push({
-                        id: id,
-                        issueDate: issueDate,
-                        state: state,
-                        products: products,
-                        supplierId: supplierId,
-                        transportNote: transportNote,
-                        skuItems: skuItems
-                    });
-                    products = [];
-                    skuItems = [];
-                    id = rows[r].ID;
-                    issueDate = rows[r].ISSUE_DATE;
-                    supplierId = rows[r].SUPPLIER_ID;
-                    state = rows[r].STATE;
-                    transportNote.deliveryDate = rows[r].TRANSPORT_NOTE;
-                    products.push({
-                        SKUId: rows[r].SKU_ID,
-                        description: rows[r].DESCRIPTION,
-                        price: rows[r].PRICE,
-                        quantity: rows[r].QUANTITY
-                    });
-                }
-            }
-            restockOrders.push({
-                id: id,
-                issueDate: issueDate,
-                state: state,
-                products: products,
-                supplierId: supplierId,
-                transportNote: transportNote,
-                skuItems: skuItems
-            });
-
+            ));
             resolve(restockOrders);
         });
     });
@@ -124,24 +69,24 @@ exports.getRestockOrders = () => {
 
 exports.getRestockOrderById = (id) => {
     return new Promise((resolve, reject) => {
-        const sql = `SELECT RO.ISSUE_DATE ISSUE_DATE, RO.ID ID, RO.STATE STATE, RO.SUPPLIER_ID SUPPILER_ID, RO.TRANSPORT_NOTE TRANSPORT_NOTE, P.DESCRIPTION DESCRIPTION, P.PRICE PRICE, P.QUANTITY QUANTITY, P.SKU_ID SKU_ID, SI.RFID RFID
-            FROM RESTOCK_ORDERS AS RO, SKU_ITEMS AS SI, (
-                SELECT I.DESCRIPTION DESCRIPTION, I.PRICE PRICE, ROI.QUANTITY QUANTITY, I.SKU_ID SKU_ID, ROI.RESTOCK_ORDER_ID RESTOCK_ORDER_ID
-                FROM ITEMS AS I, RESTOCK_ORDER_ITEM AS ROI
-                WHERE I.ID = ROI.ITEM_ID) AS P
-            WHERE P.SKU_ID = SI.SKU_ID AND RO.ID = P.RESTOCK_ORDER_ID AND RO.ID = ?`;
+        const sql = `SELECT RO.ISSUE_DATE, RO.ID, RO.STATE, RO.SUPPLIER_ID, RO.TRANSPORT_NOTE,
+                    I.DESCRIPTION, I.PRICE, ROI.QUANTITY, I.SKU_ID, SI.RFID
+                    FROM RESTOCK_ORDERS RO, ITEMS I, SKU_ITEMS SI, RESTOCK_ORDER_ITEM ROI
+                    WHERE RO.ID = ? AND RO.ID = ROI.RESTOCK_ORDER_ID AND ROI.ITEM_ID = I.ID
+                    AND I.SUPPLIER_ID = RO.SUPPLIER_ID AND I.SKU_ID = SI.SKU_ID`;
         db.all(sql, [id], (err, rows) => {
             if (err) {
                 reject(err);
                 return;
             }
-            let products = [];
-            let skuItems = [];
+            let restockOrder = {};
+            const products = [];
+            const skuItems = [];
             let id;
             let issueDate;
             let state;
             let supplierId;
-            let transportNote = {
+            const transportNote = {
                 deliveryDate: null
             };
             for (let r in rows) {
@@ -149,68 +94,163 @@ exports.getRestockOrderById = (id) => {
                 issueDate = rows[r].ISSUE_DATE;
                 state = rows[r].STATE;
                 supplierId = rows[r].SUPPLIER_ID;
-                transportNote.deliveryDate = rows[r].TRANSPORT_NOTE;
-                products.push({
-                    SKUId: rows[r].SKU_ID,
-                    description: rows[r].DESCRIPTION,
-                    price: rows[r].PRICE,
-                    quantity: rows[r].QUANTITY
-                });
-                skuItems.push({
-                    SKUId: rows[r].SKU_ID,
-                    description: rows[r].DESCRIPTION,
-                    price: rows[r].PRICE,
-                    quantity: rows[r].QUANTITY
-                });
+                if (state !== 'ISSUED')
+                    transportNote.deliveryDate = rows[r].TRANSPORT_NOTE;
+                if (products.filter(p => p.SKUId === rows[r].SKU_ID).length === 0) {
+                    products.push({
+                        SKUId: rows[r].SKU_ID,
+                        description: rows[r].DESCRIPTION,
+                        price: rows[r].PRICE,
+                        quantity: rows[r].QUANTITY
+                    });
+                }
+                if (state !== 'ISSUED' && state !== 'DELIVERY')
+                    skuItems.push({
+                        SKUId: rows[r].SKU_ID,
+                        rfid: rows[r].RFID
+                    });
             }
-            const restockOrder =
-            {
-                id: id,
-                issueDate: issueDate,
-                state: state,
-                products: products,
-                supplierId: supplierId,
-                transportNote: transportNote,
-                skuItems: skuItems
-
-            };
-
+            if (id == undefined)
+                resolve('404');
+            else if (state === 'ISSUED') {
+                restockOrder =
+                {
+                    id: id,
+                    issueDate: issueDate,
+                    state: state,
+                    products: products,
+                    supplierId: supplierId,
+                    skuItems: skuItems
+                };
+            }
+            else {
+                restockOrder =
+                {
+                    id: id,
+                    issueDate: issueDate,
+                    state: state,
+                    products: products,
+                    supplierId: supplierId,
+                    transportNote: transportNote,
+                    skuItems: skuItems
+                };
+            }
             resolve(restockOrder);
         });
     });
 }
 
+exports.checkTestResult = (item) => {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT T.RESULT
+                    FROM TEST_RESULT T, TEST_RESULT_SKUITEM TS
+                    WHERE T.RESULT = FALSE AND T.ID = TS.ID_TEST_RESULT AND TS.RFID = ?`;
+        db.all(sql, [item.rfid], (err, rows) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (rows.length > 0)
+                resolve(true);
+            else
+                resolve(false);
+        });
+    });
+}
+
+exports.createRestockOrder = (restockOrder) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'INSERT INTO RESTOCK_ORDERS(ISSUE_DATE, STATE, SUPPLIER_ID) VALUES(?, "ISSUED", ?)'
+        db.run(sql, [restockOrder.issueDate, restockOrder.supplierId], (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+        });
+        resolve(this.lastID);
+    });
+}
+
+exports.getItemBySKUId = (SKUId, supplierId) => {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT ID FROM ITEMS WHERE SKU_ID = ? AND SUPPLIER_ID = ?`;
+        db.get(sql, [SKUId, supplierId], (err, row) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            const itemId = row.ID;
+            resolve(itemId);
+        });
+    });
+}
+
+exports.createRestockOrder_join_Product = (restockOrderId, itemId, qty) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'INSERT INTO RESTOCK_ORDER_ITEM(RESTOCK_ORDER_ID, ITEM_ID, QUANTITY) VALUES(?, ?, ?)'
+        db.run(sql, [restockOrderId, itemId, qty], (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+        });
+        resolve(this.lastID);
+    });
+}
+
 exports.changeStateRestockOrder = (id, newState) => {
     return new Promise((resolve, reject) => {
-        const sql = 'UPDATE RESTOCK_ORDER SET RESTOCK_ORDER.STATE = ? WHERE RESTOCK_ORDER.ID = ?';
-        db.run(sql, [newState, id], (err) => {
+        const sql = 'UPDATE RESTOCK_ORDERS SET STATE = ? WHERE ID = ?';
+        db.run(sql, [newState, id], function (err) {
             if (err) {
                 reject(err);
                 return;
             }
             resolve(this.changes);
-
         });
     });
 }
 
-exports.createJoinSkuItems = (ROid, SIid) => {
+exports.getSKUItem = (RFID) => {
     return new Promise((resolve, reject) => {
-        const sql = 'INSERT INTO RESTOCK_ORDER_SKU_ITEM(RESTOCK_ORDER_ID, SKU_ITEM_ID) VALUES(?, ?)'
-        db.run(sql, [ROid, SIid], (err) => {
+        const sql = `SELECT * FROM SKU_ITEMS WHERE RFID = ?`;
+        db.get(sql, [RFID], (err, row) => {
             if (err) {
                 reject(err);
                 return;
             }
+            if (row == undefined)
+                resolve('404');
+            else {
+                const SKUItem = {
+                    RFID: row.RFID,
+                    SKUId: row.SKU_ID,
+                    available: row.AVAILABLE,
+                    dateOfStock: row.DATE_OF_STOCK
+                };
+                resolve(SKUItem);
+            }
         });
-        resolve(this.changes);
     });
 }
 
-exports.addTransportNoteRestockOrder = (ROid, newTransportNote) => {
+exports.addSKUItem = (RFID, SKUId) => {
     return new Promise((resolve, reject) => {
-        const sql = 'UPDATE TRANSPORT_NOTE SET RESTOCK_ORDER.TRANSPORT_NOTE = ? WHERE RESTOCK_ORDER.ID = ?';
-        db.run(sql, [newTransportNote, ROid], (err) => {
+        const sql = 'INSERT INTO SKU_ITEMS(RFID, SKU_ID, AVAILABLE) VALUES(?, ?, 1)';
+        db.run(sql, [RFID, SKUId], function (err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(this.lastID);
+        });
+    });
+}
+
+exports.addTransportNoteRestockOrder = (id, deliveryDate) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'UPDATE RESTOCK_ORDERS SET TRANSPORT_NOTE = ? WHERE ID = ?';
+        db.run(sql, [deliveryDate, id], function (err) {
             if (err) {
                 reject(err);
                 return;
@@ -222,8 +262,8 @@ exports.addTransportNoteRestockOrder = (ROid, newTransportNote) => {
 
 exports.deleteRestockOrder = (id) => {
     return new Promise((resolve, reject) => {
-        const sql = 'DELETE FROM RESTOCK_ORDERS WHERE RESTOCK_ORDERS.ID = ?';
-        db.run(sql, [id], (err) => {
+        const sql = 'DELETE FROM RESTOCK_ORDERS WHERE ID = ?';
+        db.run(sql, [id], function (err) {
             if (err) {
                 reject(err);
                 return;
@@ -233,23 +273,10 @@ exports.deleteRestockOrder = (id) => {
     });
 }
 
-exports.deleteRestockOrderJoinSkuItems = (id) => {
+exports.deleteRestockOrder_join_Product = (id) => {
     return new Promise((resolve, reject) => {
-        const sql = 'DELETE FROM RESTOCK_ORDERS_SKU_ITEMS) WHERE RESTOCK_ORDERS.ID = ?';
-        db.run(sql, [id], (err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(this.changes);
-        });
-    });
-}
-
-exports.deleteRestockOrderJoinItems = (id) => {
-    return new Promise((resolve, reject) => {
-        const sql = 'DELETE FROM RESTOCK_ORDERS_ITEMS) WHERE RESTOCK_ORDERS.ID = ?';
-        db.run(sql, [id], (err) => {
+        const sql = 'DELETE FROM RESTOCK_ORDER_ITEM WHERE RESTOCK_ORDER_ID = ?';
+        db.run(sql, [id], function (err) {
             if (err) {
                 reject(err);
                 return;
--- code/server/modules/routers/restockOrderRouter.js
+++ code/server/modules/routers/restockOrderRouter.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const dayjs = require('dayjs');
 
 const RestockOrderService = require('../services/restockOrderService');
 const db = require('../DAOs/restockOrderDAO');
@@ -7,13 +6,11 @@ const db = require('../DAOs/restockOrderDAO');
 const restockOrderService = new RestockOrderService(db);
 
 const router = express.Router();
-dayjs().format();
 
 
 //GET
 router.get('/restockOrders', async (req, res) => {
     try {
-        // 401 Unauthorized (not logged in or wrong permissions)
         const orders = await restockOrderService.getRestockOrders();
         res.status(200).json(orders);
     } catch (err) {
@@ -23,28 +20,37 @@ router.get('/restockOrders', async (req, res) => {
 
 router.get('/restockOrdersIssued', async (req, res) => {
     try {
-        const orders = await restockOrderService.getRestockOrdersIssued();
-        const restockOrdersIssued = [];
-        for (ord of orders) {
-            if (ord.state === "ISSUED")
-                restockOrdersIssued.push(ord);
-        }
-        resolve(restockOrdersIssued);
+        const restockOrders = await restockOrderService.getRestockOrders();
+        const filteredRestockORders = restockOrders.filter((f) => f.state === 'ISSUED');
+        res.status(200).json(filteredRestockORders);
     } catch (err) {
         res.status(500).json({ error: `Internal Server Error` }).end();
     }
 });
 
 router.get('/restockOrders/:id', async (req, res) => {
-    let id = Number(req.params.id);
     try {
-        // 401 Unauthorized (not logged in or wrong permissions)
-        const RestockOrder = await restockOrderService.getRestockOrderById(id);
-        // Check validation of id
-        if (RestockOrder === 'Not Found') res.status(404).json({ error: `No Restock order associated to id` }).end();
-        // 422 Unprocessable Entity (validation of id failed)
-        // END OF VALIDATION
-        res.status(200).json(RestockOrder);
+        const RestockOrder = await restockOrderService.getRestockOrderById(req.params.id);
+        if (RestockOrder === '422')
+            res.status(422).json({ error: `validation of id failed` }).end();
+        else if (RestockOrder === '404')
+            return res.status(404).json({ error: `no restock order associated to id` }).end();
+        else
+            res.status(200).json(RestockOrder);
+    } catch (err) {
+        res.status(500).json({ error: `Internal Server Error` }).end();
+    }
+});
+
+router.get('/restockOrders/:id/returnItems', async (req, res) => {
+    try {
+        const items = await restockOrderService.getSKUItemsToBeReturned(req.params.id);
+        if (items === '422')
+            res.status(422).json({ error: `validation of id failed or restock order state != COMPLETEDRETURN` }).end();
+        else if (items === '404')
+            return res.status(404).json({ error: `no restock order associated to id` }).end();
+        else
+            res.status(200).json(items);
     } catch (err) {
         res.status(500).json({ error: `Internal Server Error` }).end();
     }
@@ -54,29 +60,11 @@ router.get('/restockOrders/:id', async (req, res) => {
 //POST
 router.post('/restockOrder', async (req, res) => {
     try {
-        // 401 Unauthorized (not logged in or wrong permissions)
-        // Check validation of request body
-        if (!req.body) return res.status(422).json({ error: `Validation of request body failed` }).end();
-        let RestockOrder = req.body;
-        // if (!(RestockOrder && RestockOrder.RestockDate && RestockOrder.products && RestockOrder.restockOrderId))
-        //     Restock res.status(422).json({ error: `Validation of request body failed` }).end();
-        // Check number of elements of the request 
-        if (Object.entries(RestockOrder).length !== 3) return res.status(422).json({ error: `Validation of request body failed` }).end();
-        // Check date validation
-        if (!dayjs(RestockOrder.RestockDate).isValid()) return res.status(422).json({ error: `Validation of request body failed` }).end();
-        // Check length of the list of products
-        // if (RestockOrder.products.length === 0) Restock res.status(422).json({ error: `Validation of request body failed` }).end();
-        // Check products type
-        // Check restockOrderId type
-        // 404 Not Found (no restock order associated to restockOrderId)
-        // END OF VALIDATION
-        let RestockOrderId = await restockOrderService.createRestockOrder(RestockOrder);
-        for (let i of RestockOrder.products) {
-            await restockOrderService.createRestockOrder_join_Product(i.SKUId, RestockOrderId)
-        }
-        // revert in case if order created but join not
-        return res.status(201).end();
-
+        const status = await restockOrderService.createRestockOrder(req.body);
+        if (status === '422')
+            res.status(422).json({ error: `validation of request body failed` }).end();
+        else
+            return res.status(201).end();
     } catch (err) {
         res.status(503).json({ error: `Internal Server Error` }).end();
     }
@@ -86,15 +74,13 @@ router.post('/restockOrder', async (req, res) => {
 // PUT 
 router.put('/restockOrder/:id', async (req, res) => {
     try {
-        let id = Number(req.params.id);
-
-        if (id <= 0)
-            res.status(422).json({ error: 'Unprocessable Entity' });
-        const order = await restockOrderService.getRestockOrderById(id);
-        if (order === 'not found')
-            res.status(404).json({ error: 'Not Found' }).end();
-
-        await restockOrderService.changeStateRestockOrder(id, req.body.newState);
+        const status = await restockOrderService.changeStateRestockOrder(req.params.id, req.body);
+        if (status === '422')
+            res.status(422).json({ error: `validation of request body or of id failed` }).end();
+        else if (status === '404')
+            return res.status(404).json({ error: `no restock order associated to id` }).end();
+        else
+            return res.status(200).end();
     } catch (error) {
         res.status(503).json({ error: `Internal Server Entity` }).end();
     }
@@ -102,18 +88,13 @@ router.put('/restockOrder/:id', async (req, res) => {
 
 router.put('/restockOrder/:id/skuItems', async (req, res) => {
     try {
-        let id = Number(req.params.id);
-
-        if (id <= 0)
-            res.status(422).json({ error: 'Unprocessable Entity' });
-        const order = await restockOrderService.getRestockOrderById(id);
-        if (order === 'not found')
-            res.status(404).json({ error: 'Not Found' }).end();
-
-        if (req.body.newState === "COMPLETED") {
-
-        }
-        await restockOrderService.addSkuItemsList(req.body.skuItems, id);
+        const status = await restockOrderService.addSkuItemsList(req.params.id, req.body);
+        if (status === '422')
+            res.status(422).json({ error: `validation of request body or of id failed or order state != DELIVERED` }).end();
+        else if (status === '404')
+            return res.status(404).json({ error: `no restock order associated to id` }).end();
+        else
+            return res.status(200).end();
     } catch (error) {
         res.status(503).json({ error: `Service Unavailable` }).end();
     }
@@ -121,18 +102,13 @@ router.put('/restockOrder/:id/skuItems', async (req, res) => {
 
 router.put('/restockOrder/:id/transportNote', async (req, res) => {
     try {
-        let id = Number(req.params.id);
-
-        if (id <= 0)
-            res.status(422).json({ error: 'Unprocessable Entity' });
-        const order = await restockOrderService.getInternalRestockById(id);
-        if (order === 'not found')
-            res.status(404).json({ error: 'Not Found' }).end();
-        await restockOrderService.addTransportNoteRestockOrder(id, req.body.transportNote.deliveryDate)
-        if (req.body.newState === "COMPLETED") {
-
-        }
-        await restockOrderService.addTransportNoteRestockOrder(req.body.transportNote, id);
+        const status = await restockOrderService.addTransportNoteRestockOrder(req.params.id, req.body);
+        if (status === '422')
+            res.status(422).json({ error: `validation of request body or of id failed or order state != DELIVERY or deliveryDate is before issueDate` }).end();
+        else if (status === '404')
+            return res.status(404).json({ error: `no restock order associated to id` }).end();
+        else
+            return res.status(200).end();
     } catch (error) {
         res.status(503).json({ error: `Service Unavailable` }).end();
     }
@@ -140,16 +116,12 @@ router.put('/restockOrder/:id/transportNote', async (req, res) => {
 
 //DELETE
 router.delete('/restockOrder/:id', async (req, res) => {
-    let id = Number(req.params.id);
     try {
-        // 401 Unauthorized (not logged in or wrong permissions)
-        let deletedElelements = await restockOrderService.deleteRestockOrder(id);
-        let deletedJoin1 = await restockOrderService.deleteRestockOrderJoinSkuItems(id);
-        let deletedJoins = await restockOrderService.deleteRestockOrderJoinItems(id);
-        // Check id validation
-        if (deletedElelements === 0 || deletedJoins === 0 || deletedJoin1 === 0) res.status(422).json({ error: `Validation of id failed` }).end();
-        // END OF VALIDATION
-        res.status(204).end();
+        const status = await restockOrderService.deleteRestockOrder(req.params.id);
+        if (status === '422')
+            res.status(422).json({ error: `validation of id failed` }).end();
+        else
+            res.status(204).end();
     } catch (err) {
         res.status(500).json({ error: `Service Unavailable` }).end();
     }
