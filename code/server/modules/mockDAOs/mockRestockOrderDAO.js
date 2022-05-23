fea619f70d08ef61f378bc6ddb3a1454a195e60f
Arefeh Mohammad Nejad s301416 <s301416@studenti.polito.it>
2022-05-23 14:10:35 +0430

    mockRestockOrderDAO added

---
+++ code/server/modules/mockDAOs/mockRestockOrderDAO.js
@@ -0,0 +1,7 @@
+exports.getRestockOrders = jest.fn();
+exports.getRestockOrderById = jest.fn();
+exports.createRestockOrder = jest.fn();
+exports.changeStateRestockOrder = jest.fn();
+exports.addTransportNoteRestockOrder = jest.fn();
+exports.deleteRestockOrder = jest.fn();
+exports.deleteRestockOrders = jest.fn();
