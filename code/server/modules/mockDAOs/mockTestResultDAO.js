33854652c431d3910b25df93d6e6d9cf9f457214
LUCA CALO' s294828 <s294828@studenti.polito.it>
2022-05-25 01:04:06 +0200

    test testResult implemented

--- code/server/modules/mockDAOs/mockTestResultDAO.js
+++ code/server/modules/mockDAOs/mockTestResultDAO.js
@@ -3,4 +3,3 @@ exports.getTestResultByRfidAndId = jest.fn();
 exports.createTestResult = jest.fn();
 exports.modifyTestResult = jest.fn();
 exports.deleteTestResult = jest.fn();
-exports.deleteTestResults = jest.fn();
